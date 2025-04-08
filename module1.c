366d47cc9bfb604856035b9c4a5cd83fdd22515a
shojol <sh0j0l3875891@gmail.com>
2025-04-08 20:50:31 +0600

    module 3

---
+++ module1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char name = 'm';
+
+    printf("Hello World\n");
+    printf( "%c", name);
+    return 0;
+};
---
+++ module3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 0; i < 10000000; i++)
+    {
+        printf("Hello World %d\n", i++);
+    }
+
+    return 0;
+}
