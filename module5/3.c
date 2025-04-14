f144a349ea725421373a4bc078366ac0083a5006
shojol <sh0j0l3875891@gmail.com>
2025-04-15 01:32:00 +0600

    module 5

---
+++ module5/3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    int a = x / 1000;
+
+    if (a % 2 == 0)
+    {
+        printf("EVEN");
+    }
+    else
+    {
+        printf("ODD");
+    }
+
+    return 0;
+};
